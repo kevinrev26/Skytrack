715b95d62c47edabeb9c06011f2c1f74284e33dd
Kevin Rivera <kevinrev26@gmail.com>
2025-05-09 22:27:41 -0600

    Wall blocker movement logic added

---
+++ main/Wall/Wall.go
@@ -0,0 +1,15 @@
+components {
+  id: "Wall"
+  component: "/main/Wall/Wall.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"Asset 14\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/Wall/Wall.atlas\"\n"
+  "}\n"
+  ""
+}
